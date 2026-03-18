7065591a80f9a9dd44596b32c2cd6782ad79278a
William Partridge <william.partridge@student.hhs.school.nz>
2026-03-18 15:12:08 +1300

    apple

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pygame
+import random
 
 clock = pygame.time.Clock()
-FPS = 60
+FPS = 20
 
 scr_x=640 ; scr_y=480
 fullscreen = False
 done = False
 x, y = 0, 0
+eaten = False
+apple_count = 0
+ax = 0
+ay = 0
 
 pygame.init()
 
@@ -18,8 +23,22 @@ else: screen = pygame.display.set_mode((scr_x, scr_y), pygame.RESIZABLE)
 player_surf = pygame.Surface((10, 10))
 player_surf.fill((0, 255, 0))
 
+apple_surf = pygame.Surface((10, 10))
+apple_surf.fill((255, 0, 0))
+
 snake_x = [0]
 snake_y = [0]
+direction_x = 1
+direction_y = 0
+
+def apple():
+    ax = random.randint(0, round(scr_x/10))
+    ay = random.randint(0, round(scr_y/10))
+    print(ax, ay)
+    print((10*ax, 10*ay))
+    return (ax, ay)
+
+    
 
 while not done:
     for event in pygame.event.get():
@@ -30,25 +49,38 @@ while not done:
                 done = True
             
             if event.key == pygame.K_w:
-                snake_y.insert(0, snake_y[0] - 1)
-                snake_x.insert(0, snake_x[0])
+                direction_x = 0
+                direction_y = -1
             if event.key == pygame.K_s:
-                snake_y.insert(0, snake_y[0] + 1)
-                snake_x.insert(0, snake_x[0])
+                direction_x = 0
+                direction_y = 1
             if event.key == pygame.K_d:
-                snake_x.insert(0, snake_x[0] + 1)
-                snake_y.insert(0, snake_y[0])
+                direction_x = 1
+                direction_y = 0
             if event.key == pygame.K_a:
-                snake_x.insert(0, snake_x[0] - 1)
-                snake_y.insert(0, snake_y[0])
-        
-    print(snake_x, snake_y)
+                direction_x = -1
+                direction_y = 0
 
-    
+    snake_y.insert(0, snake_y[0] + direction_y)
+    snake_x.insert(0, snake_x[0] + direction_x)
+    if not eaten:
+        snake_x.pop()
+        snake_y.pop()
 
     screen.fill((0, 0, 0))
+    if apple_count <= 0:
+        print("hejhehe")
+        ax, ay = apple()
+        print(ax,ay)
+        apple_count += 1
+    screen.blit(player_surf, (10*ax, 10*ay))
+
     for seg in range(len(snake_x)):
         screen.blit(player_surf, (10*snake_x[seg], 10*snake_y[seg]))
+
+    # screen.blit(player_surf, (180, 290))
+    
+
 
     pygame.display.flip()
     clock.tick(FPS)
