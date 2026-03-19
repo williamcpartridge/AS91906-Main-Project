ef54dc3e511ee33eede18d2038598010cb478b58
William Partridge <william.partridge@student.hhs.school.nz>
2026-03-20 11:06:16 +1300

    bg prototype and slit fr from main game loop

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import random
 
 clock = pygame.time.Clock()
-FPS = 10
+FPS = 60
 
 scr_x=640 ; scr_y=480
 fullscreen = False
@@ -11,6 +11,7 @@ x, y = 0, 0
 eaten = False
 ax = 0
 ay = 0
+input_num = 0
 
 pygame.init()
 
@@ -25,12 +26,31 @@ player_surf.fill((0, 255, 0))
 apple_surf = pygame.Surface((10, 10))
 apple_surf.fill((255, 0, 0))
 
+bg_surf = pygame.Surface((10, 10))
+bg_surf.fill((10, 255, 10))
+
 snake_x = [0]
 snake_y = [0]
 direction_x = 1
 direction_y = 0
+tile  = 1
+fc = 0
 
 apple_list = []
+
+
+def game_init():
+    tile = 1
+    for x in range(round(scr_x/10)):
+        for y in range(round(scr_y/10)):
+            if tile % 2 == 0:
+                bg_surf.fill((10, 230, 10))
+            else:
+                bg_surf.fill((30, 220, 30))
+
+            screen.blit(bg_surf, (x*10, y*10))
+            tile += 1
+
 
 def apple():
     ax = 10*random.randint(0, round(scr_x/10))
@@ -38,8 +58,11 @@ def apple():
     return (ax, ay)
 
     
+game_init()
 
 while not done:
+    input_num = 0
+    fc += 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
@@ -47,35 +70,42 @@ while not done:
             if event.key == pygame.K_ESCAPE:
                 done = True
             
-            if event.key == pygame.K_w:
-                direction_x = 0
-                direction_y = -1
-            if event.key == pygame.K_s:
-                direction_x = 0
-                direction_y = 1
-            if event.key == pygame.K_d:
-                direction_x = 1
-                direction_y = 0
-            if event.key == pygame.K_a:
-                direction_x = -1
-                direction_y = 0
+            if input_num == 0:
+                if direction_y != 1 and event.key == pygame.K_w:
+                    direction_x = 0
+                    direction_y = -1
+                    input_num = 1
+                if direction_y != -1 and event.key == pygame.K_s:
+                    direction_x = 0
+                    direction_y = 1
+                    input_num = 1
+                if direction_x != -1 and event.key == pygame.K_d:
+                    direction_x = 1
+                    direction_y = 0
+                    input_num = 1
+                if direction_x != 1 and event.key == pygame.K_a:
+                    direction_x = -1
+                    direction_y = 0
+                    input_num = 1
 
-    if snake_x[0] + direction_x in snake_x and snake_y[0] + direction_y in snake_y:
-        done = True
-    elif snake_x[0] + direction_x < 0 or snake_y[0] + direction_y < 0 or snake_x[0] + direction_x > (scr_x-10)/10 or snake_y[0] + direction_y > (scr_y-10)/10:
-        done = True
-    else:
-        snake_y.insert(0, snake_y[0] + direction_y)
-        snake_x.insert(0, snake_x[0] + direction_x)
+    if not fc%6:
+        if snake_x[0] + direction_x in snake_x and snake_y[0] + direction_y in snake_y:
+            done = True
+        elif snake_x[0] + direction_x < 0 or snake_y[0] + direction_y < 0 or snake_x[0] + direction_x > (scr_x-10)/10 or snake_y[0] + direction_y > (scr_y-10)/10:
+            done = True
+        else:
+            snake_y.insert(0, snake_y[0] + direction_y)
+            snake_x.insert(0, snake_x[0] + direction_x)
 
-
-    if not eaten:
-        snake_x.pop()
-        snake_y.pop()
+        if not eaten:
+            snake_x.pop()
+            snake_y.pop()
     
-    eaten = False
+        eaten = False
 
     screen.fill((0, 0, 0))
+    game_init()
+
     if len(apple_list) <= 0:
         ax, ay = apple()
         apple_list.append((ax, ay))
